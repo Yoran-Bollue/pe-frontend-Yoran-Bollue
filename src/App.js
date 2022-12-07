8a6c082d00ded1a0bbbd2f59e911d8198d818198
Yoran-Bollue <71662215+Yoran-Bollue@users.noreply.github.com>
2022-12-07 22:43:08 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import CollectionForm from './Form'
 
 class App extends Component{
 
-    API_URL="http://193.191.176.16:8080"
+    API_URL="193.191.176.16:8080"
 
     state = {
       products: [],
